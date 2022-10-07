ab76a95aee60157ca522fb1f6f3a4c968ff1ae8b
Valentina Jaramillo Raquejo <valjaramilloraq@gmail.com>
2022-10-06 21:10:57 -0500

    Created a project in Intellij to test the candidate datasets

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bigdataprogram-applaudo
 A repository for the Big Data Program - Applaudo Studios - Valentina Jaramillo Raquejo
+Notes: The dataset that is being used for the project is https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2010-to-Present/erm2-nwe9 . This dataset is stored in the directory dataset_test/src/main/resources/data locally.
---
+++ dataset_test/src/main/scala/Demo1.scala
@@ -0,0 +1,66 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+object Demo1 extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Demo")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+    spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+
+  val serviceRequestsSchema = StructType(Array(
+    StructField("unique_key", StringType),
+    StructField("created_date", TimestampType),
+    StructField("closed_date", TimestampType),
+    StructField("agency", StringType),
+    StructField("agency_name", StringType),
+    StructField("complaint_type", StringType),
+    StructField("descriptor", StringType),
+    StructField("location_type", StringType),
+    StructField("incident_zip", StringType),
+    StructField("incident_address", StringType),
+    StructField("street_name", StringType),
+    StructField("cross_street_1", StringType),
+    StructField("cross_street_2", StringType),
+    StructField("intersection_street_1", StringType),
+    StructField("intersection_street_2", StringType),
+    StructField("address_type", StringType),
+    StructField("city", StringType),
+    StructField("landmark", StringType),
+    StructField("facility_type", StringType),
+    StructField("status", StringType),
+    StructField("due_date", TimestampType),
+    StructField("resolution_description", StringType),
+    StructField("resolution_action_updated_date", TimestampType),
+    StructField("community_board", StringType),
+    StructField("bbl", StringType),
+    StructField("borough", StringType),
+    StructField("x_coordinate", DoubleType),
+    StructField("y_coordinate", DoubleType),
+    StructField("open_data_channel_type", StringType),
+    StructField("park_facility_name", StringType),
+    StructField("park_borough", StringType),
+    StructField("vehicle_type", StringType),
+    StructField("taxi_company_borough", StringType),
+    StructField("taxi_pick_up_location", StringType),
+    StructField("bridge_highway_name", StringType),
+    StructField("bridge_highway_direction", StringType),
+    StructField("road_ramp", StringType),
+    StructField("bridge_highway_segment", StringType),
+    StructField("latitude", DoubleType),
+    StructField("longitude", DoubleType),
+    StructField("location", StringType)
+  ))
+
+  val serviceRequestsDF = spark.read
+    .schema(serviceRequestsSchema)
+    .option("timestampFormat", "MM/dd/YYYY hh:mm:ss aa")
+    .option("header", "true")
+    .option("sep", ",")
+    .option("nullValue", "")
+    .csv("src/main/resources/data/311_Service_Requests_from_2010_to_Present.csv")
+
+  serviceRequestsDF.show()
+}
